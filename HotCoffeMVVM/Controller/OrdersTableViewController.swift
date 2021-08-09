42d0f45d2458e4a52e048ca66dfd3aaef26febf0
Paulo Junior <jr@live.at>
2021-08-09 13:10:04 -0300

    Displaying Orders in UITableView

--- HotCoffeMVVM/Controller/OrdersTableViewController.swift
+++ HotCoffeMVVM/Controller/OrdersTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    var orderListViewModel = OrderListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populateOrders()
@@ -26,10 +28,30 @@ class OrdersTableViewController: UITableViewController {
         WebService().load(resource: resource) { result in
             switch result {
                 case .success(let orders):
-                    print(orders)
+                    self.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                    self.tableView.reloadData()
                 case .failure(let error):
                     print(error)
             }
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
     }
 }
