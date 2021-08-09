7d484f39f691f46c5d797163a60a8154f0b1269c
Paulo Junior <jr@live.at>
2021-08-09 10:54:08 -0300

    Creating Models

---
+++ HotCoffeMVVM/Models/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  HotCoffeMVVM
+//
+//  Created by Junior Ferreira on 09/08/21.
+//
+
+import Foundation
+
+enum CoffeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order {
+    let name: String
+    let email: String
+    let type: CoffeType
+    let size: CoffeSize
+}
